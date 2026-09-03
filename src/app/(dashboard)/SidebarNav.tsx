5885676a413deabd81f39fbc22584d70854ab137
Janani Admin <admin@jananihomefoods.com>
2026-09-03 15:21:54 +0530

    Fix duplicate ICONS import

--- src/app/(dashboard)/SidebarNav.tsx
+++ src/app/(dashboard)/SidebarNav.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { useState, useEffect } from 'react'
-import { ICONS } from './themes/icons'
 import {
   applyFestivalTheme,
   updateThemeEffect,
